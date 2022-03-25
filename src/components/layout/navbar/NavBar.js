835744ed8a2a712b409e89d768b48f3166a3c304
mel-liow <mel.liow.94@gmail.com>
2022-03-25 14:14:46 -0700

    feat: add generic components, styles

---
+++ src/components/layout/navbar/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { Navbar, Nav } from 'react-bootstrap';
+
+import './NavBar.css'
+
+export const NavBar = () => {
+
+    return (
+        <Navbar bg="light" expand="lg">
+            <div className="nav-bar">
+                <Navbar.Brand href="#home">Dashboard</Navbar.Brand>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="nav-items">
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#contributors">Contributors</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </div>
+        </Navbar>
+    )
+
+
+}
