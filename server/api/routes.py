85855c8a557f62f2a018358e5baef0afda24cfb9
mel-liow <mel.liow.94@gmail.com>
2022-03-25 14:14:06 -0700

    feat: working api call to server

--- server/api/routes.py
+++ server/api/routes.py
@@ -1,3 +1,11 @@
 from flask import request
 
 from api import app
+import json
+
+
+@app.route("/predict", methods=["POST"])
+def get_predictions():
+    data = request.json
+    print("DATA", data)
+    return data
--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { Header, NavBar } from "./components/layout";
 
-function App() {
+import { ThemeContext } from './context'
+
+import "./App.css"
+
+
+const App = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false)
+  const [input, setInput] = useState('')
+
+  const setTheme = () => {
+    setIsDarkTheme(state => !state);
+  }
+
+  const handleChange = (e) => {
+    const input = e.target.value
+    setInput(input)
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    return fetch("/predict", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ input }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('data', data)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeContext.Provider
+      value={{ isDarkTheme, toggleTheme: setTheme }}
+    >
+      <div className={isDarkTheme ? "app-dark" : "app"}>
+        <Header>
+          <NavBar />
+        </Header>
+        <form className="form" onSubmit={handleSubmit}>
+          <label className="label">Simple input</label>
+          <div>
+            <input
+              type="text"
+              name="input"
+              value={input}
+              onChange={handleChange}
+            />
+            <input type="submit" value="Submit" />
+          </div>
+        </form>
+      </div>
+    </ThemeContext.Provider>
+  )
 }
 
-export default App;
+export default App
