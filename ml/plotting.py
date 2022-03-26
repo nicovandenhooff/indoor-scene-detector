9cdb19b5b5ff73e36e5d8de6f795d7263df55a88
Nico Van den Hooff <nicovandenhooff@gmail.com>
2022-03-26 14:34:13 -0700

    feat: create plotting module

---
+++ ml/plotting.py
@@ -0,0 +1,57 @@
+import torch
+import matplotlib.pyplot as plt
+
+
+def visualize_model(model, dl, device, num_images=6):
+    """Visualize predictions from a PyTorch model via a DataLoader.
+
+    Note: This function is intended for use with assessing model predictions
+          rather than real time inference.
+
+    Parameters
+    ----------
+    model : torch.nn.Module
+        A trained PyTorch model.
+    dl : torch.utils.data.DataLoader
+        A PyTorch DataLoader.
+    device : str
+        The current device ("cpu" or "cuda")
+    num_images : int, optional
+        Number of images to plot (must be even)
+    """
+
+    if num_images % 2 != 0:
+        raise ValueError("num_images must be positive")
+
+    plotted = 0
+    class_names = dl.dataset.dataset.classes
+    fig, axes = plt.subplots(num_images // 2, 2, figsize=(15, 15))
+
+    if model.training:
+        model.eval()
+
+    with torch.no_grad():
+        for inputs, labels in dl:
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            outputs = model(inputs)
+            probs = torch.softmax(outputs, 1)
+            pred_probs, pred_labels = torch.max(probs, 1)
+
+            for j in range(inputs.size()[0]):
+                img = inputs[j].permute(1, 2, 0)
+                label = class_names[labels[j]]
+                pred_label = class_names[pred_labels[j].item()]
+                pred_prob = pred_probs[j].item()
+
+                title = f"Predicted: {pred_label}\nActual: {label}\n Probability: {pred_prob:.5f}"
+
+                axes[plotted // 2, plotted % 2].imshow(img)
+                axes[plotted // 2, plotted % 2].axis("off")
+                axes[plotted // 2, plotted % 2].set_title(title)
+
+                plotted += 1
+
+                if plotted == num_images:
+                    plt.tight_layout()
+                    return
