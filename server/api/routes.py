d474e68a449cf93995130127991a49f684a255d6
Nico Van den Hooff <nicovandenhooff@gmail.com>
2022-03-27 15:16:47 -0700

    refactor: update get_predictions

--- server/api/routes.py
+++ server/api/routes.py
@@ -1,37 +1,20 @@
 from flask import request
-
-# from ..ml import prediction
-from ml import prediction, training
-
+from ml import prediction
 from api import app
-import json
-import base64
-import torch
+
+MODELS = prediction.load_models()
 
 
 @app.route("/predict", methods=["POST"])
 def get_predictions():
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
     data = request.json
-    # print("DATA", data)
 
-    img_b64 = data["newImage"].split(",")[1]
-    img_bytes = base64.b64decode(img_b64)
+    # convert base 64 image to tensor
+    img_bytes = prediction.b64_to_bytes(data["newImage"])
     img_tensor = prediction.transform_image(img_bytes)
 
-    if data["network"] == "alexnet":
-        model = training.get_custom_densenet121(n_classes=10, pretrained=False)
-        training.load_weights(
-            model,
-            "/Users/nicovandenhooff/Dropbox/GITHUB/cnn-dashboard/server/ml/model_weights/DenseNet121_Tuned.pth",
-            device,
-        )
-
+    # choose model and get outputs
+    model = MODELS[data["network"]]
     pred_prob, pred_label, pred_class = prediction.get_prediction(model, img_tensor)
 
     return {"prob": pred_prob, "label": pred_label, "class": pred_class}
-
-    # response = {"server_response": data}
-    # return response
