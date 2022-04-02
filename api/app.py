b6f2ad86821df002f8273c6bf103067731fe25ce
Nico Van den Hooff <nicovandenhooff@gmail.com>
2022-04-02 15:57:21 -0700

    chore: comment out ml code pre-deployment

--- api/app.py
+++ api/app.py
@@ -5,7 +5,7 @@ from ml import prediction
 app = Flask(__name__)
 CORS(app)
 
-MODELS = prediction.load_models()
+# MODELS = prediction.load_models()
 
 # if __name__ == "__main__":
 #     app.run(port="5001", debug=True, host="0.0.0.0")
@@ -17,21 +17,21 @@ def home():
     return {"hello": "HIIIII"}
 
 
-@app.route("/api/v1.0/predict", methods=["POST"])
-@cross_origin()
-def get_predictions():
-    data = request.json
-    img_bytes = prediction.b64_to_bytes(data["newImage"])
-    img_tensor = prediction.transform_image(img_bytes)
+# @app.route("/api/v1.0/predict", methods=["POST"])
+# @cross_origin()
+# def get_predictions():
+#     data = request.json
+#     img_bytes = prediction.b64_to_bytes(data["newImage"])
+#     img_tensor = prediction.transform_image(img_bytes)
 
-    model_name = data["network"]
+#     model_name = data["network"]
 
-    # TODO: move network name to react
-    if data["network"] != "simple_cnn":
-        model_name += "_" + data["transferLearning"]
+#     # TODO: move network name to react
+#     if data["network"] != "simple_cnn":
+#         model_name += "_" + data["transferLearning"]
 
-    # choose model and get outputs
-    model = MODELS[model_name]
-    pred_prob, pred_label, pred_class = prediction.get_prediction(model, img_tensor)
+#     # choose model and get outputs
+#     model = MODELS[model_name]
+#     pred_prob, pred_label, pred_class = prediction.get_prediction(model, img_tensor)
 
-    return {"prob": pred_prob, "label": pred_label, "class": pred_class}
+#     return {"prob": pred_prob, "label": pred_label, "class": pred_class}
