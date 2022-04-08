7c939300b0bcdaeb28eb219ab486e782177eb001
mel-liow <mel.liow.94@gmail.com>
2022-04-07 18:34:05 -0700

    feat: refactor form and app.js

--- client/src/App.js
+++ client/src/App.js
@@ -16,13 +16,6 @@ import { GlobalStyles } from './global';
 const App = () => {
   const [image, setImage] = useState()
   const [imageURL, setImageURL] = useState('')
-  const [network, setNetwork] = useState('alexnet')
-  const [transferLearning, setTransferLearning] = useState('tuned')
-  const [loading, setLoading] = useState(false)
-
-  const [postImage, setPostImage] = useState({
-    myFile: "",
-  });
   const [predictions, setPredictions] = useState({})
 
   const { theme } = useContext(ThemeContext)
@@ -33,44 +26,6 @@ const App = () => {
     const imageUrl = URL.createObjectURL(image)
     setImageURL(imageUrl)
   }, [image])
-
-  const handleSubmit = (e) => {
-    if (!image || !network) {
-      return toggle(true)
-    }
-    setLoading(true)
-    e.preventDefault();
-
-    return axios.post('/api/predict', {
-      newImage: postImage, network, transferLearning
-    }).then((res) => {
-      setPredictions(res.data)
-      setLoading(false)
-    }).catch(e => {
-      console.log(`error = ${e}`)
-    })
-  };
-
-  const convertToBase64 = (file) => {
-    return new Promise((resolve, reject) => {
-      const fileReader = new FileReader();
-      fileReader.readAsDataURL(file);
-      fileReader.onload = () => {
-        resolve(fileReader.result);
-      };
-      fileReader.onerror = (error) => {
-        reject(error);
-      };
-    });
-  };
-
-
-  const handleFileUpload = async (e) => {
-    const file = e.target.files[0];
-    const base64 = await convertToBase64(file);
-    setPostImage(base64);
-    setImage(file)
-  };
 
 
   return (
@@ -100,12 +55,10 @@ const App = () => {
           <Body>
             <Panel className="panel-form">
               <Form
-                handleSubmit={handleSubmit}
-                setImageURL={setImageURL}
-                setNetwork={setNetwork}
-                setImage={(e) => handleFileUpload(e)}
-                setTransferLearning={setTransferLearning}
-                loading={loading}
+                setImage={setImage}
+                image={image}
+                toggle={toggle}
+                setPredictions={setPredictions}
               />
             </Panel>
             <Panel>
--- client/src/components/form/Form.js
+++ client/src/components/form/Form.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import {
     Button,
     CircularProgress,
@@ -7,14 +7,60 @@ import {
     Radio,
     RadioGroup
 } from "@mui/material";
-
+import axios from '../../axios';
 
 import "./Form.css"
 
-export const Form = ({ handleSubmit, setImage, setNetwork, setTransferLearning, loading }) => {
+export const Form = ({ image, toggle, setImage, setPredictions }) => {
 
-    const handleNetworkChange = (e) => setNetwork(e.target.value)
-    const handleTransferLearningChange = (e) => setTransferLearning(e.target.value)
+    const [network, setNetwork] = useState('alexnet')
+    const [transferLearning, setTransferLearning] = useState('tuned')
+    const [postImage, setPostImage] = useState({ myFile: "", });
+    const [loading, setLoading] = useState(false)
+
+    const handleFileUpload = async (e) => {
+        const file = e.target.files[0];
+        const base64 = await convertToBase64(file);
+        setPostImage(base64);
+        setImage(file)
+    };
+
+    const convertToBase64 = (file) => {
+        return new Promise((resolve, reject) => {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(file);
+            fileReader.onload = () => {
+                resolve(fileReader.result);
+            };
+            fileReader.onerror = (error) => {
+                reject(error);
+            };
+        });
+    };
+
+    const handleNetworkChange = (e) =>
+        setNetwork(e.target.value)
+
+    const handleTransferLearningChange = (e) =>
+        setTransferLearning(e.target.value)
+
+    const handleSubmit = (e) => {
+        if (!image || !network) {
+            return toggle(true)
+        }
+        setLoading(true)
+        e.preventDefault();
+
+        return axios.post('/api/predict', {
+            newImage: postImage, network, transferLearning
+        }).then((res) => {
+            setPredictions(res.data)
+            setLoading(false)
+        }).catch(e => {
+            console.log(`error = ${e}`)
+        })
+    };
+
 
     const submitButton = () => {
         return !loading ?
@@ -34,7 +80,7 @@ export const Form = ({ handleSubmit, setImage, setNetwork, setTransferLearning, 
                 label="Image"
                 name="myFile"
                 accept=".jpeg, .png, .jpg"
-                onChange={setImage}
+                onChange={handleFileUpload}
             />
             Select a network:
             <RadioGroup
