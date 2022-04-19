cfc5aca8db2589bd65c704cbd4d5be4671053e8f
nicovandenhooff <78452264+nicovandenhooff@users.noreply.github.com>
2022-04-19 04:50:08 +0000

    Deploying to gh-pages from @ nicovandenhooff/cnn-dashboard@1bc817b8e863bfda8577a43f18f0df92b85de59b 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cnn-dashboard/precache-manifest.776771e283cec93f5c1cc16a89ba27ba.js"
+  "/precache-manifest.1e32578632a337c45d0ed4dfc4a16944.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cnn-dashboard/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
