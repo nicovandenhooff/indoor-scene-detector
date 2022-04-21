f1840294936706525bbe26c56e1b3f0d585bbd16
Nico Van den Hooff <nicovandenhooff@gmail.com>
2022-04-21 07:52:52 -0700

    refactor: clean up app.py

--- api/app.py
+++ api/app.py
@@ -21,20 +21,20 @@ def hello():
 @app.route("/api/predict", methods=["POST"])
 @cross_origin()
 def get_predictions():
+    # get data and convert base64 to tensor
     data = request.json
     img_bytes = prediction.b64_to_bytes(data["image"])
     img_tensor = prediction.transform_image(img_bytes)
-    model_name = data["modelName"]
 
-    # choose model and get outputs
+    # choose model
+    model_name = data["modelName"]
     model = MODELS[model_name]
 
-    # get top 3 predictions
+    # get top 3 predictions then wrangle for FE
     pred_probs, pred_labels, pred_classes = prediction.get_topk_predictions(
         model, img_tensor, k=3
     )
 
-    # wrangle format of predictions for JSON
     predictions = prediction.wrangle_topk_predictions(
         pred_probs, pred_labels, pred_classes
     )
